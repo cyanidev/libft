7afafb0431af43e5d9b063dc9ec434f368477cfe
cyanidev <93842463+cyanidev@users.noreply.github.com>
2022-04-27 23:43:42 +0200

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-static int	count_words(const char *str, char c)
+static int	word_count(const char *str, char c)
 {
 	int	count;
 	int	flag;
@@ -37,13 +37,15 @@ static int	count_words(const char *str, char c)
 	return (count);
 }
 
-static char	*word_dup(const char *str, int start, int end)
+static char	*ft_word(const char *str, int start, int end)
 {
 	char	*word;
 	int		i;
 
 	i = 0;
 	word = malloc(sizeof(char) * ((end - start) + 1));
+	if (word == NULL)
+		return (NULL);
 	while (start < end)
 	{
 		word[i] = str[start];
@@ -53,39 +55,43 @@ static char	*word_dup(const char *str, int start, int end)
 	word[i] = '\0';
 	return (word);
 }
-/*
-void	ft_free(char **s, int i)
+
+static void	*ft_free(char **split, int i)
 {
-	while (--i >0)
-		free(s[i]);
-	free(s);
+	while (i > 0)
+	{
+		free(split[i - 1]);
+		i--;
+	}
+	free(split);
 	return (NULL);
-} 
-*/
+}
+
 char	**ft_split(char const *s, char c)
 {
-	size_t	i;
-	size_t	j;
-	int		start;
-	char	**result;
+	size_t		i;
+	size_t		j;
+	int			start;
+	const char	**result = malloc(sizeof(char *) * (word_count(s, c) + 1));
 
-	result = malloc(sizeof(char *) * (count_words(s, c) + 1));
-	if (s == NULL || result == NULL)
-		return (NULL);
 	i = 0;
 	j = 0;
 	start = -1;
+	if (result == NULL || s == NULL)
+		return (NULL);
 	while (i <= ft_strlen(s))
 	{
 		if (s[i] != c && start < 0)
 			start = i;
 		else if ((s[i] == c || i == ft_strlen(s)) && start >= 0)
 		{
-			result[j++] = word_dup(s, start, i);
+			result[j++] = ft_word(s, start, i);
+			if (result[j - 1] == NULL)
+				return (ft_free((char **)result, j));
 			start = -1;
 		}
 		i++;
 	}
 	result[j] = 0;
-	return (result);
+	return ((char **)result);
 }
